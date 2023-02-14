a42bfd834f59101f70e44e723f37c30974822fe6
KASH <113846702+projectkash69@users.noreply.github.com>
2023-02-14 14:35:21 +0530

    Add files via upload

---
+++ IoT-Attendance-System-using-RFID-main/send_data_to_google_sheet/send_data_to_google_sheet.ino
@@ -0,0 +1,179 @@
+#include <SPI.h>
+#include <MFRC522.h>
+#include <Arduino.h>
+#include <ESP8266WiFi.h>
+#include <ESP8266WiFiMulti.h>
+#include <ESP8266HTTPClient.h>
+#include <WiFiClient.h>
+#include <WiFiClientSecureBearSSL.h>
+
+// Fingerprint for demo URL, expires on June 2, 2021, needs to be updated well before this date
+const uint8_t fingerprint[20] = {0x4a, 0x2e, 0xb2, 0xa8, 0x29, 0x12, 0x9a, 0xca, 0xac, 0xe1, 0xe0, 0xf4, 0xa0, 0x6c, 0x74, 0x4b, 0x4b, 0x7d, 0x5b, 0xab};
+// 4a 2e b2 a8 29 12 9a ca ac e1 e0 f4 a0 6c 74 4b 4b 7d 5b ab
+
+#define RST_PIN  D3     // Configurable, see typical pin layout above
+#define SS_PIN   D4     // Configurable, see typical pin layout above
+#define BUZZER   D2     // Configurable, see typical pin layout above
+
+MFRC522 mfrc522(SS_PIN, RST_PIN);  // Instance of the class
+MFRC522::MIFARE_Key key;  
+ESP8266WiFiMulti WiFiMulti;
+MFRC522::StatusCode status;      
+
+/* Be aware of Sector Trailer Blocks */
+int blockNum = 2;  
+
+/* Create another array to read data from Block */
+/* Legthn of buffer should be 2 Bytes more than the size of Block (16 Bytes) */
+byte bufferLen = 18;
+byte readBlockData[18];
+
+String data2;
+const String data1 = "https://script.google.com/macros/s/AKfycbwi5Lioy_QXdIw0Snh3x_9bsLDtOu2-nxgSBgGmj9k8DPO1j_g0-cM3N9PIgehlKRZN/exec?name=";
+
+void setup() 
+{
+  /* Initialize serial communications with the PC */
+  Serial.begin(9600);
+  // Serial.setDebugOutput(true);
+
+  Serial.println();
+  Serial.println();
+  Serial.println();
+
+  for (uint8_t t = 4; t > 0; t--) 
+  {
+    Serial.printf("[SETUP] WAIT %d...\n", t);
+    Serial.flush();
+    delay(1000);
+  }
+
+  WiFi.mode(WIFI_STA);
+  
+  /* Put your WIFI Name and Password here */
+  WiFiMulti.addAP("iot", "project1234");
+
+  /* Set BUZZER as OUTPUT */
+  pinMode(BUZZER, OUTPUT);
+  /* Initialize SPI bus */
+  SPI.begin();
+}
+
+void loop()
+{
+  /* Initialize MFRC522 Module */
+  mfrc522.PCD_Init();
+  /* Look for new cards */
+  /* Reset the loop if no new card is present on RC522 Reader */
+  if ( ! mfrc522.PICC_IsNewCardPresent())
+  {
+    return;
+  }
+  /* Select one of the cards */
+  if ( ! mfrc522.PICC_ReadCardSerial()) 
+  {
+    return;
+  }
+  /* Read data from the same block */
+  Serial.println();
+  Serial.println(F("Reading last data from RFID..."));
+  ReadDataFromBlock(blockNum, readBlockData);
+  /* If you want to print the full memory dump, uncomment the next line */
+  //mfrc522.PICC_DumpToSerial(&(mfrc522.uid));
+  
+  /* Print the data read from block */
+  Serial.println();
+  Serial.print(F("Last data in RFID:"));
+  Serial.print(blockNum);
+  Serial.print(F(" --> "));
+  for (int j=0 ; j<16 ; j++)
+  {
+    Serial.write(readBlockData[j]);
+  }
+  Serial.println();
+  digitalWrite(BUZZER, HIGH);
+  delay(200);
+  digitalWrite(BUZZER, LOW);
+  delay(200);
+  digitalWrite(BUZZER, HIGH);
+  delay(200);
+  digitalWrite(BUZZER, LOW);
+  
+  // wait for WiFi connection
+  if ((WiFiMulti.run() == WL_CONNECTED)) 
+  {
+    std::unique_ptr<BearSSL::WiFiClientSecure>client(new BearSSL::WiFiClientSecure);
+
+    client->setFingerprint(fingerprint);
+    // Or, if you happy to ignore the SSL certificate, then use the following line instead:
+    // client->setInsecure();
+
+    data2 = data1 + String((char*)readBlockData);
+    data2.trim();
+    Serial.println(data2);
+    
+    HTTPClient https;
+    Serial.print(F("[HTTPS] begin...\n"));
+    if (https.begin(*client, (String)data2))
+    {  
+      // HTTP
+      Serial.print(F("[HTTPS] GET...\n"));
+      // start connection and send HTTP header
+      int httpCode = https.GET();
+    
+      // httpCode will be negative on error
+      if (httpCode > 0) 
+      {
+        // HTTP header has been send and Server response header has been handled
+        Serial.printf("[HTTPS] GET... code: %d\n", httpCode);
+        // file found at server
+      }
+      else 
+      {
+        Serial.printf("[HTTPS] GET... failed, error: %s\n", https.errorToString(httpCode).c_str());
+      }
+      https.end();
+      delay(1000);
+    } 
+    else 
+    {
+      Serial.printf("[HTTPS} Unable to connect\n");
+    }
+  }
+}
+
+void ReadDataFromBlock(int blockNum, byte readBlockData[]) 
+{ 
+  /* Prepare the ksy for authentication */
+  /* All keys are set to FFFFFFFFFFFFh at chip delivery from the factory */
+  for (byte i = 0; i < 6; i++)
+  {
+    key.keyByte[i] = 0xFF;
+  }
+  /* Authenticating the desired data block for Read access using Key A */
+  status = mfrc522.PCD_Authenticate(MFRC522::PICC_CMD_MF_AUTH_KEY_A, blockNum, &key, &(mfrc522.uid));
+
+  if (status != MFRC522::STATUS_OK)
+  {
+     Serial.print("Authentication failed for Read: ");
+     Serial.println(mfrc522.GetStatusCodeName(status));
+     return;
+  }
+  else
+  {
+    Serial.println("Authentication success");
+  }
+
+  /* Reading data from the Block */
+  status = mfrc522.MIFARE_Read(blockNum, readBlockData, &bufferLen);
+  if (status != MFRC522::STATUS_OK)
+  {
+    Serial.print("Reading failed: ");
+    Serial.println(mfrc522.GetStatusCodeName(status));
+    return;
+  }
+  else
+  {
+    Serial.println("Block was read successfully");  
+  }
+}
